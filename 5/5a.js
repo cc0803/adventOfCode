fdeb0e9ed8d1a43ce2a7b82fae708caaf4dc357c
cc0803 <simongeldbach@gmail.com>
2025-03-15 12:03:30 +0100

    Create Function to make printerArguments readable

--- 5/5a.js
+++ 5/5a.js
@@ -1,56 +1,33 @@
+const { constants } = require("buffer");
 const fs = require("fs");
+
+function readFile(path) {
+	try {
+		let data = fs.readFileSync(path);
+		return data.toString().split("");
+	} catch (error) {
+		throw new Error("Failed to read file");
+	}
+}
 
 let rulesRaw = readFile("./test5rules.txt");
 let printerRaw = readFile("./test5print.txt");
 
-let rules = [];
 let printer = [];
-cleanRules();
-cleanPrinter();
+let buffer = [];
+let str = "";
+for (let i = 0; i < printerRaw.length; i++) {
+	if (printerRaw[i] == ",") {
+		buffer.push(str);
+		str = "";
+	} else if (printerRaw[i] == "\n") {
+		buffer.push(str);
+		printer.push(buffer);
+		buffer = [];
+		str = "";
+	} else {
+		str += printerRaw[i];
+	}
+}
+
 console.log(printer);
-
-function cleanPrinter() {
-	let buffer = [];
-	let el = [];
-
-	for (let i = 0; i < printerRaw.length; i++) {
-		if (printerRaw[i] == ",") {
-			buffer.push(Number(el.join("")));
-			el = [];
-		} else if (printerRaw[i] == "\n") {
-			buffer.push(Number(el.join("")));
-			printer.push(buffer);
-			buffer = [];
-			el = [];
-		} else {
-			el.push(printerRaw[i]);
-		}
-	}
-}
-
-function cleanRules() {
-	let buffer = [];
-	let el = [];
-	for (let i = 0; i < rulesRaw.length; i++) {
-		if (rulesRaw[i] == "|") {
-			buffer.push(Number(el.join("")));
-			el = [];
-		} else if (rulesRaw[i] == "\n") {
-			buffer.push(Number(el.join("")));
-			rules.push(buffer);
-			buffer = [];
-			el = [];
-		} else {
-			el.push(rulesRaw[i]);
-		}
-	}
-}
-
-function readFile(path) {
-	try {
-		const data = fs.readFileSync(path);
-		return data.toString().split("");
-	} catch (error) {
-		throw new Error(error);
-	}
-}
